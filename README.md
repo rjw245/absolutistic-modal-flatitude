3ad960981187ea7fafc9d2e4b01d2d751e40e8bb
Riley Wood <rjw245@gmail.com>
2017-03-05 16:25:36 -0500

    First working version.
    Cycles through all scales, starting with the C major scale.
    I'm pretty proud of this. It has a list of all possible notes
    in the C scale, as well as the pattern of which notes to play
    and which not to. I exploit absolutistic modal flatitude in that
    we always flat the 4th note in any full-full-full-half step pattern
    (steps between notes). An example of a full step is from C to D
    (skipping C#) and an example of a half step is E to F (no note
    inbetween).
    I find this full-full-full-half pattern wherever it is in my
    larger pattern of the current scale (it may wrap around the edge)
    and I flat that particular note, and continue playing.
    Once I go over the edge (transition past Locrian) my new root WOULD be
    D, but I rotate my pattern back around so that my root continues to be
    the lower C note.

---
+++ README.md
@@ -0,0 +1,7 @@
+Requires pyaudio, which depends on portaudio for Linux.
+
+You can install portaudio with dev headers like so:
+
+~~~~
+sudo apt-get install portaudio19-dev
+~~~~
---
+++ absolutistic_modal_flatitude.py
@@ -0,0 +1,100 @@
+import math
+import pyaudio
+from collections import OrderedDict, deque
+from itertools import cycle
+
+
+def contains(small, big):
+    big_cycle = list(big) + list(big)
+    for i in xrange(0, len(big)):
+        for j in xrange(len(small)):
+            if big_cycle[i+j] != small[j]:
+                break
+        else:
+            return i, i+len(small)
+    return False
+
+frequencies = OrderedDict()
+frequencies['C5'] = 523.25
+frequencies['C#'] = 554.37
+frequencies['D'] = 587.33
+frequencies['D#'] = 622.25
+frequencies['E'] = 659.25
+frequencies['F'] = 698.46
+frequencies['F#'] = 739.99
+frequencies['G'] = 783.99
+frequencies['G#'] = 830.61
+frequencies['A'] = 880.0
+frequencies['A#'] = 932.33
+frequencies['B'] = 987.77
+frequencies['C6'] = 1046.50
+
+
+class Step:
+    FullStep, HalfStep, Endpt = range(3)
+
+to_play = deque([True,
+                 False,
+                 True,
+                 False,
+                 True,
+                 True,
+                 False,
+                 True,
+                 False,
+                 True,
+                 False,
+                 True])
+
+notes_to_play = 13
+
+while True:
+    # Play one scale
+    for note_idx in xrange(0, notes_to_play):
+        should_play = to_play[note_idx % len(to_play)]
+        if should_play:
+            # sudo apt-get install python-pyaudio
+            PyAudio = pyaudio.PyAudio
+
+            # See http://en.wikipedia.org/wiki/Bit_rate#Audio
+            BITRATE = 16000  # number of frames per second/frameset.
+
+            # See http://www.phy.mtu.edu/~suits/notefreqs.html
+            FREQUENCY = frequencies.values()[note_idx % len(frequencies)]
+            LENGTH = .5  # seconds to play sound
+
+            NUMBEROFFRAMES = int(BITRATE * LENGTH)
+            RESTFRAMES = NUMBEROFFRAMES % BITRATE
+            WAVEDATA = ''
+
+            for x in xrange(NUMBEROFFRAMES):
+                WAVEDATA = WAVEDATA+chr(int(math.sin(x/((BITRATE/FREQUENCY)/math.pi))*127+128))    
+
+            # fill remainder of frameset with silence
+            for x in xrange(RESTFRAMES):
+                WAVEDATA = WAVEDATA+chr(128)
+
+            p = PyAudio()
+            stream = p.open(format=p.get_format_from_width(1),
+                            channels=1,
+                            rate=BITRATE,
+                            output=True)
+            stream.write(WAVEDATA)
+            stream.stop_stream()
+            stream.close()
+            p.terminate()
+
+
+    # Shift notes around in the scale
+    pattern = [False, True,  # Full step
+               False, True,  # Full step
+               False, True,  # Full step
+               True]         # Half step
+    (start, end) = contains(pattern, to_play)
+    temp = to_play[(end-3) % len(to_play)]
+    to_play[(end-3) % len(to_play)] = to_play[(end-2) % len(to_play)]
+    to_play[(end-2) % len(to_play)] = temp
+
+    # Must always play the first note, keep it a C scale
+    if not to_play[0]:
+        to_play.rotate(1)
