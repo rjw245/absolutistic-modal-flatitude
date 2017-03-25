377a703bd4d8b590bd08f37ee7960692478ae7bf
Riley Wood <rjw245@gmail.com>
2017-03-24 23:30:47 -0400

    Put while loop in "main"

--- absolutistic_modal_flatitude.py
+++ absolutistic_modal_flatitude.py
@@ -73,31 +73,32 @@ cur_mode = deque([True,
                   True])
 
 
-while True:
-    # Play one scale
-    for note_idx in xrange(0, len(frequencies)):
-        should_play = cur_mode[note_idx % len(cur_mode)]
-        if should_play:
-            freq = frequencies.values()[note_idx % len(frequencies)]
-            play_note(freq, 0.3)
+if __name__ == "__main__":
+    while True:
+        # Play one scale
+        for note_idx in xrange(0, len(frequencies)):
+            should_play = cur_mode[note_idx % len(cur_mode)]
+            if should_play:
+                freq = frequencies.values()[note_idx % len(frequencies)]
+                play_note(freq, 0.3)
 
-    # Find the full-full-full-half-step subpattern
-    # in the current mode (5 note subpattern)
-    subpattern = [False, True,  # Full step (one note unplayed, next played)
-                  False, True,  # Full step
-                  False, True,  # Full step
-                  True]         # Half step
-    (start, end) = contains(subpattern, cur_mode)
+        # Find the full-full-full-half-step subpattern
+        # in the current mode (5 note subpattern)
+        subpattern = [False, True,  # Full step (one unplayed, next played)
+                      False, True,  # Full step
+                      False, True,  # Full step
+                      True]         # Half step
+        (start, end) = contains(subpattern, cur_mode)
 
-    # Flat the 4th note of the subpattern to move us to a new mode
-    # of this scale
-    temp = cur_mode[(end-3) % len(cur_mode)]
-    cur_mode[(end-3) % len(cur_mode)] = cur_mode[(end-2) % len(cur_mode)]
-    cur_mode[(end-2) % len(cur_mode)] = temp
+        # Flat the 4th note of the subpattern to move us to a new mode
+        # of this scale
+        temp = cur_mode[(end-3) % len(cur_mode)]
+        cur_mode[(end-3) % len(cur_mode)] = cur_mode[(end-2) % len(cur_mode)]
+        cur_mode[(end-2) % len(cur_mode)] = temp
 
-    # Make sure C stays the root. If we wrap around modes, shift
-    # back to a C scale.
-    if not cur_mode[0]:
-        cur_mode.rotate(1)
+        # Make sure C stays the root. If we wrap around modes, shift
+        # back to a C scale.
+        if not cur_mode[0]:
+            cur_mode.rotate(1)
 
-    time.sleep(1)
+        time.sleep(1)
